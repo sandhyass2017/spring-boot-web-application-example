3445b0044444cae80fdcfad7504ba34859becfe9
sandhyass2017 <sandhya.sujathasoman@cognizant.com>
2018-04-20 13:30:16 +0530

    mvc config update

--- src/main/java/com/boraji/tutorial/springboot/ApplicationWebMvcConfig.java
+++ src/main/java/com/boraji/tutorial/springboot/ApplicationWebMvcConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter{
+	 private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+	            "classpath:/resources/", "classpath:/static/", "classpath:/public/","classpath:/templates/"};
 
 	@Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -19,8 +22,12 @@ public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter{
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
+        //resolver.setPrefix("src/main/resources/templates/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
